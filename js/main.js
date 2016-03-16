11d6704110d0d48dea0aeaac36754f8a3a4f8b4e
Matheus Pavanelli <mspavanelli@gmail.com>
2016-03-16 17:04:16 -0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/*
+Este arquivo ainda não faz nada. Aguarde as próximas sessões do WebDev sobre JavaScript!
+Experimente tirar o comentário da linha 7 e carregar novamente o index.html
+*/
+
+var mensagem = 'Bem-vindo!';
+// alert(mensagem);
